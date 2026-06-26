452f4e1b5cf7e8d61bcc42556ac61df851ea2a2e
Muhammad Rizwan Ali <mrrizwanaali@gmail.com>
2026-06-26 22:30:15 +0500

    server and db setup

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,26 @@
-import express from 'express';
+require('dotenv').config();
+import express, { NextFunction,Request, Response } from 'express';
 export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errorMiddleware } from './middleware/error';
+
+app.use(express.json({limit: '50mb'}));
+
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.ORIGIN,
+}))
+
+app.get('/test', (req:Request, res:Response,next:NextFunction) => {
+    res.status(200).json({message: 'API is working'});
+})
+
+app.all('*', (req:Request, res:Response,next:NextFunction) => {
+    const err = new Error('Route ${req.originalUrl} not found') as any;
+    err.status = 404;
+    next(err);
+})
+
+
+app.use(errorMiddleware);
