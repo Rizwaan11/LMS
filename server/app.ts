4572a73b153e4806b9d99507ea58319f53fa670b
Muhammad Rizwan Ali <mrrizwanaali@gmail.com>
2026-06-30 00:56:08 +0500

    course and notification module

--- server/app.ts
+++ server/app.ts
@@ -5,6 +5,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { errorMiddleware } from './middleware/error';
 import userRouter from './routes/user.routes';
+import courseRouter from './routes/course.routes';
+import notificationRoute from './routes/notification.routes';
+
+
 app.use(express.json({limit: '50mb'}));
 
 app.use(cookieParser());
@@ -14,6 +18,9 @@ app.use(cors({
 
 //routes
 app.use('/api/v1/',userRouter);
+app.use('/api/v1/',courseRouter);
+app.use('/api/v1/',notificationRoute);
+
 
 app.get('/test', (req:Request, res:Response,next:NextFunction) => {
     res.status(200).json({message: 'API is working'});
